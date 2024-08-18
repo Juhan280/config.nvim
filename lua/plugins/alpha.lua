73d0d714efdb80cf23d9ad8e464419346fe95cae
Juhan280 <92646057+Juhan280@users.noreply.github.com>
2024-08-19 03:13:29 +0600

    add border on LspInfo and add `h` command for Alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -24,9 +24,10 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("e", "  > New file", ":ene<CR>"),
 			dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
 			dashboard.button("r", "󰈢  > Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("h", "󰋖  > Help", ":Telescope help_tags<CR>"),
 			dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 			dashboard.button("q", "󰈆  > Quit NVIM", ":qa<CR>"),
 		}
@@ -39,15 +40,12 @@ return {
 		vim.api.nvim_create_autocmd("VimEnter", {
 			once = true,
 			callback = function()
-				math.randomseed(os.time())
 				local fg_color = "#" .. string.format("%06x", math.random(0, 0xFFFFFF))
 				vim.api.nvim_set_hl(0, "AlphaHeader", { fg = fg_color })
 			end,
 		})
 
 		-- Disable folding on alpha buffer
-		vim.cmd([[
-			autocmd FileType alpha setlocal nofoldenable
-		]])
+		vim.cmd("autocmd FileType alpha setlocal nofoldenable")
 	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
 
 		require("neoconf").setup()
 
+		require('lspconfig.ui.windows').default_options.border = "rounded"
 		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 			border = "rounded",
 		})
