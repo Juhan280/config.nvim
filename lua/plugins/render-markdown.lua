0059eb07ac6698f41a84f5f8edcdaa6d65d8e839
Juhan280 <92646057+Juhan280@users.noreply.github.com>
2024-08-22 02:41:59 +0600

    update: markdown.nvim -> render-markdown.nvim

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,10 +1,11 @@
 return { --- @type LazySpec
-	"MeanderingProgrammer/markdown.nvim",
+	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter", -- Mandatory
 		"nvim-tree/nvim-web-devicons",   -- Optional but recommended
 	},
 	ft = "markdown",
 	opts = { ---@type render.md.UserConfig
+		preset = "lazy",
 	},
 }
