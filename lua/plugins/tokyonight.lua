c9d91ee9a4f6442b8e788862430274f766eccf38
Juhan280 <92646057+Juhan280@users.noreply.github.com>
2024-08-23 15:41:58 +0600

    update colorscheme transparency for desktop

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,12 +3,15 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		---@diagnostic disable-next-line: missing-fields
 		require("tokyonight").setup({
 			style = "night",
-			transparent = false,
+			transparent = not vim.os.isAndroid(),
 			styles = {
 				comments = { italic = not vim.os.isAndroid() },
 				keywords = { italic = not vim.os.isAndroid() },
+				sidebars = vim.os.isAndroid() and "dark" or "transparent",
+				floats = vim.os.isAndroid() and "dark" or "transparent",
 			},
 		})
 		vim.cmd.colorscheme("tokyonight")
