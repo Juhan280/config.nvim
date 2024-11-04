29e3224fbc3a0acb67dc77aae24a5c4d1fd1ea45
Juhan Ahmed <azurarobo280@gmail.com>
2024-11-04 22:56:08 +0600

    feat: add keybind `<leader>fo` for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,11 @@
-return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "echasnovski/mini.icons" },
-	lazy = false
+return { ---@type LazySpec
+	'stevearc/oil.nvim',
+	-- Optional dependencies
+	dependencies = { "echasnovski/mini.icons" },
+	cmd = "Oil",
+	keys = { { "<leader>fo", desc = "Open oil buffer" } },
+	config = function()
+		require("oil").setup()
+		vim.keymap.set("n", "<leader>fo", ":Oil<cr>", { desc = "Open oil buffer" })
+	end
 }
