3c1945977465227ee4a3428d234a4a242edfa884
Juhan Ahmed <azurarobo280@gmail.com>
2024-11-04 23:04:57 +0600

    fix: don't lazy load oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,10 +2,7 @@ return { ---@type LazySpec
 	'stevearc/oil.nvim',
 	-- Optional dependencies
 	dependencies = { "echasnovski/mini.icons" },
-	cmd = "Oil",
-	keys = { { "<leader>fo", desc = "Open oil buffer" } },
-	config = function()
-		require("oil").setup()
-		vim.keymap.set("n", "<leader>fo", ":Oil<cr>", { desc = "Open oil buffer" })
-	end
+	lazy = false, -- required to open oil directly when passing a directory as the argument
+	opts = {},
+	keys = { { "<leader>fo", ":Oil<cr>", desc = "Open oil buffer" } }
 }
