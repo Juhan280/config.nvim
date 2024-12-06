18f82ac4e821c093de6e402422d90e9f7b8ad9b1
Juhan Ahmed <azurarobo280@gmail.com>
2024-12-06 22:31:36 +0600

    feat(queries): add more captures for gleam

--- queries/gleam/textobjects.scm
+++ queries/gleam/textobjects.scm
@@ -12,6 +12,17 @@
 	"}"
 	(#make-range! "function.inner" @_start @_end))
 
+(anonymous_function) @function.outer
+
+(anonymous_function
+	"{"
+	.
+	(_) @_start @_end
+	(_)? @_end
+	.
+	"}"
+	(#make-range! "function.inner" @_start @_end))
+
 ; quantifies as class(es)
 (type_definition) @class.outer
 (data_constructors) @class.inner
@@ -22,7 +33,14 @@
 
 ; blocks
 (block) @block.outer
-; TODO add @block.inner
+(block
+	"{"
+	.
+	(_) @_start @_end
+	(_)? @_end
+	.
+	"}"
+	(#make-range! "block.inner" @_start @_end))
 
 ; calls
 (function_call) @call.outer
@@ -39,87 +57,41 @@
     (#make-range! "call.inner" @_start @_end)))
 
 ; comments
-(statement_comment) @comment.outer
+[
+ (module_comment)
+ (statement_comment)
+ (comment)
+] @comment.outer
 
 ; parameter
-[
- (function_parameters)
- (arguments)
- (data_constructor_arguments)
-] @parameter.outer
+(( ","? @_start
+	 . 
+	 [
+		(function_parameter)
+		(argument)
+		(type_argument)
+		(data_constructor_argument)
+	] @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
 
-[
- (function_parameter)
- (argument)
- (data_constructor_argument)
-] @parameter.inner
+((tuple
+	 ","? @_start
+	 . 
+	 (identifier) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
 
+((tuple_pattern
+	 ","? @_start
+	 . 
+	 (identifier) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
 
-; ((type_parameters
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((type_parameters
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((tuple_pattern
-;   "," @_start
-;   .
-;   (identifier) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((tuple_pattern
-;   .
-;   (identifier) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((tuple_struct_pattern
-;   "," @_start
-;   .
-;   (identifier) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((tuple_struct_pattern
-;   .
-;   (identifier) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; (tuple_expression
-;   "," @_start
-;   .
-;   (_) @parameter.inner
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; (tuple_expression
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((tuple_type
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((tuple_type
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
+((record_pattern_arguments
+	 ","? @_start
+	 . 
+	 (record_pattern_argument) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+
 ; (struct_item
 ;   body: (field_declaration_list
 ;     "," @_start
@@ -149,78 +121,11 @@
 ;     .
 ;     ","? @_end
 ;     (#make-range! "parameter.outer" @parameter.inner @_end)))
-;
-; ((closure_parameters
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((closure_parameters
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((arguments
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((arguments
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((type_arguments
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((type_arguments
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; ((token_tree
-;   "," @_start
-;   .
-;   (_) @parameter.inner)
-;   (#make-range! "parameter.outer" @_start @parameter.inner))
-;
-; ((token_tree
-;   .
-;   (_) @parameter.inner
-;   .
-;   ","? @_end)
-;   (#make-range! "parameter.outer" @parameter.inner @_end))
-;
-; (scoped_use_list
-;   list: (use_list
-;     "," @_start
-;     .
-;     (_) @parameter.inner
-;     (#make-range! "parameter.outer" @_start @parameter.inner)))
-;
-; (scoped_use_list
-;   list: (use_list
-;     .
-;     (_) @parameter.inner
-;     .
-;     ","? @_end
-;     (#make-range! "parameter.outer" @parameter.inner @_end)))
-;
-; [
-;   (integer_literal)
-;   (float_literal)
-; ] @number.inner
+
+[
+  (integer)
+  (float)
+] @number.inner
 
 (let
   pattern: (_) @assignment.inner @assignment.lhs
