90a65f38f9eb4bfcf6e79e465859a62dd61a9d9a
Juhan Ahmed <azurarobo280@gmail.com>
2025-05-13 17:19:04 +0600

    chore(lsp): migrate to Neovim 0.11+ LSP config and Mason v2 API
    - Updated to use `vim.lsp.config()` and `vim.lsp.enable()` for LSP setup
    - Removed `mason-lspconfig.setup_handlers` usage
    - Switched to `mason-org` plugins as part of v2.0 migration
    - Updated plugin lockfile to reflect latest compatible versions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,9 +1,9 @@
 return { ---@type LazySpec
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		{ "williamboman/mason.nvim", opts = {} },
-		"williamboman/mason-lspconfig.nvim",
-		{ "nvimtools/none-ls.nvim",  config = function() require("config.null-ls") end },
+		{ "mason-org/mason.nvim",           opts = {} },
+		{ "mason-org/mason-lspconfig.nvim", opts = {} },
+		{ "nvimtools/none-ls.nvim",         config = function() require("config.null-ls") end },
 		"Issafalcon/lsp-overloads.nvim",
 
 		"folke/neoconf.nvim",
@@ -12,8 +12,6 @@ return { ---@type LazySpec
 	},
 	lazy = false,
 	config = function()
-		local lspconfig = require("lspconfig")
-		local masonlsp = require("mason-lspconfig")
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		local ok, nvim_cmp = pcall(require, "cmp_nvim_lsp")
 		if ok then
@@ -23,36 +21,28 @@ return { ---@type LazySpec
 		require("config.lsp-attach")
 		-- require("neoconf").setup()
 
-		masonlsp.setup()
-
-		local set_servers = {}
-
-		masonlsp.setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-
-				set_servers[server_name] = 1
-			end,
-
-			jsonls = function()
-				lspconfig.jsonls.setup({
-					settings = {
-						json = {
-							schemas = require('schemastore').json.schemas(),
-							validate = { enable = true },
-						},
-					},
-				})
-			end
-		})
-
 		-- servers that are not installed by mason but exists in the PATH
 		local servers = {
-			clangd = {},
-			gleam = {},
-			lua_ls = {},
+			"clangd",
+			"gleam",
+			"lua_ls",
+			"rust_analyzer",
+			"taplo",
+			"tinymist",
+		}
+
+		---@type { [string]: vim.lsp.ClientConfig }
+		---@diagnostic disable: missing-fields
+		local configs = {
+			["*"] = { capabilities = capabilities },
+			jsonls = {
+				settings = {
+					json = {
+						schemas = require('schemastore').json.schemas(),
+						validate = { enable = true },
+					},
+				}
+			},
 			rust_analyzer = {
 				settings = {
 					['rust-analyzer'] = {
@@ -64,22 +54,17 @@ return { ---@type LazySpec
 					}
 				},
 			},
-			taplo = {},
 			tinymist = {
 				settings = {
 					formatterMode = "typstyle",
 				}
 			},
 		}
+		---@diagnostic enable: missing-fields
 
-		for server_name, server in pairs(servers) do
-			if set_servers[server_name] ~= nil then goto continue end
-
-			server.capabilities = vim.tbl_deep_extend('force', capabilities, server.capabilities or {})
-			lspconfig[server_name].setup(server)
-			set_servers[server_name] = 1
-
-			::continue::
+		for name, config in pairs(configs) do
+			vim.lsp.config(name, config)
 		end
+		vim.lsp.enable(servers)
 	end,
 }
