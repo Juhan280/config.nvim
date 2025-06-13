d283e6063194ab72bfc97c8b5e24cb4df76de4c7
Juhan Ahmed <azurarobo280@gmail.com>
2025-06-14 01:16:23 +0600

    refactor(treesitter): switch to main branch from master

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,66 @@
----@module "nvim-treesitter"
 return { ---@type LazySpec
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		-- Plugin for `textobjects`
-		"nvim-treesitter/nvim-treesitter-textobjects"
-	},
-	main = "nvim-treesitter.configs",
-	version = false,
-	lazy = false,
-	build = ":TSUpdate",
-	---@diagnostic disable-next-line: missing-fields
-	opts = { ---@type TSConfig
-		auto_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		version = false,
+		lazy = false,
+		build = ":TSUpdate",
+		-- TODO: check if anything else is necessary to do
+		-- opts = { ---@type TSConfig
+		-- 	highlight = { enable = true },
+		-- 	indent = { enable = true },
+		-- }
+		config = function()
+			-- Auto install logic
+			local nvim_treesitter = require("nvim-treesitter")
+			local available = nvim_treesitter.get_available()
+			local function try_install_curr_lang()
+				local installed = nvim_treesitter.get_installed()
+				local lang = vim.bo.filetype
+				if vim.tbl_contains(available, lang) and not vim.tbl_contains(installed, lang) then
+					nvim_treesitter.install(lang):await(function()
+						vim.notify(string.format("Installed treesitter '%s' parser", lang), vim.log.levels.INFO)
+						vim.treesitter.start(0, lang)
+					end)
+				end
+			end
 
-				keymaps = {
-					af = { query = "@function.outer", desc = "around function" },
-					["if"] = { query = "@function.inner", desc = "inside function" },
-					ac = { query = "@class.outer", desc = "around class region" },
-					ic = { query = "@class.inner", desc = "inside class region" },
-					aP = { query = "@parameter.outer", desc = "around parameter" },
-					iP = { query = "@parameter.inner", desc = "inside parameter" },
+			try_install_curr_lang()
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "*" },
+				group = vim.api.nvim_create_augroup("NvimTreesitter-auto_install", { clear = true }),
+				callback = try_install_curr_lang,
+			})
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
 				},
-			},
-		},
+			})
+
+			---@param lhs string
+			---@param query_string string
+			---@param desc string?
+			local function mapxo(lhs, query_string, desc)
+				vim.keymap.set({ "x", "o" }, lhs, function()
+					require("nvim-treesitter-textobjects.select")
+							.select_textobject(query_string, "textobjects")
+				end, { desc = desc })
+			end
+
+			mapxo("af", "@function.outer", "around function")
+			mapxo("if", "@function.inner", "inside function")
+			mapxo("ac", "@class.outer", "around class region")
+			mapxo("ic", "@class.inner", "inside class region")
+			mapxo("aP", "@parameter.outer", "around parameter")
+			mapxo("iP", "@parameter.inner", "inside parameter")
+		end
 	}
 }
