ac7978c2dc1a4963db5752c34640983e1e1ec62e
Juhan Ahmed <azurarobo280@gmail.com>
2025-06-23 01:00:58 +0600

    refactor(alpha): replace `:` with `<cmd>`

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,7 +1,7 @@
 local function get_git_button(dashboard)
 	local gitpath = vim.uv.cwd() .. "/.git"
 	if (vim.uv or vim.loop).fs_stat(gitpath) then
-		return dashboard.button("g", "󰊢  > Git", ":Neogit<CR>")
+		return dashboard.button("g", "󰊢  > Git", "<cmd>Neogit<CR>")
 	end
 end
 
@@ -12,9 +12,7 @@ return {
 	},
 	lazy = false,
 	config = function()
-		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
-		local fortune = require("alpha.fortune")
 
 		-- Set header
 		dashboard.section.header.val = {
@@ -31,28 +29,23 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New file", ":ene<CR>"),
-			dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
-			dashboard.button("r", "󰈢  > Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("e", "  > New file", "<cmd>ene<CR>"),
+			dashboard.button("f", "󰈞  > Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("r", "󰈢  > Recent", "<cmd>Telescope oldfiles<CR>"),
 			get_git_button(dashboard),
-			dashboard.button("h", "󰋖  > Help", ":Telescope help_tags<CR>"),
-			dashboard.button("l", "  > Lazy", ":Lazy<CR>"),
-			dashboard.button("c", "  > Config", ":e $MYVIMRC | :cd %:p:h |  pwd<CR>"),
-			dashboard.button("q", "󰈆  > Quit", ":qa<CR>"),
+			dashboard.button("h", "󰋖  > Help", "<cmd>Telescope help_tags<CR>"),
+			dashboard.button("l", "  > Lazy", "<cmd>Lazy<CR>"),
+			dashboard.button("c", "  > Config", "<cmd>e $MYVIMRC | cd %:p:h |  pwd<CR>"),
+			dashboard.button("q", "󰈆  > Quit", "<cmd>qa<CR>"),
 		}
 
-		dashboard.section.footer.val = fortune()
+		dashboard.section.footer.val = require("alpha.fortune")()
 
 		-- Send config to alpha
-		alpha.setup(dashboard.opts)
+		require("alpha").setup(dashboard.opts)
 
-		vim.api.nvim_create_autocmd("VimEnter", {
-			once = true,
-			callback = function()
-				local fg_color = "#" .. string.format("%06x", math.random(0, 0xFFFFFF))
-				vim.api.nvim_set_hl(0, "AlphaHeader", { fg = fg_color })
-			end,
-		})
+		local fg_color = "#" .. string.format("%06x", math.random(0, 0xFFFFFF))
+		vim.api.nvim_set_hl(0, "AlphaHeader", { fg = fg_color })
 
 		-- Disable folding on alpha buffer
 		vim.cmd("autocmd FileType alpha setlocal nofoldenable")
