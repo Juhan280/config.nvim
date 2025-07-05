9593b0da65810efa704e209dcd948967de9ec89d
Juhan Ahmed <azurarobo280@gmail.com>
2025-07-05 19:05:21 +0600

    refactor: remove `nvim-web-devicons` and replace it with `mini.icons`

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -35,5 +35,17 @@ return { ---@type LazySpec
 			search_method = "cover_or_next",
 		},
 	},
-	{ "echasnovski/mini.icons", opts = {} },
+	{
+		"echasnovski/mini.icons",
+		specs = {
+			{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+		opts = {},
+	},
 }
