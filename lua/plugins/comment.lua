1a1ef4b75612a55b4d6dcf78410a05e37df5e8af
Juhan280 <azurarobo280@gmail.com>
2025-07-09 03:58:02 +0600

    feat(plugins): add `folke/todo-comments.nvim`

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,13 +1,23 @@
 return { ---@type LazySpec
-	"numToStr/Comment.nvim",
-	version = false,
-	keys = {
-		{ "gcc", mode = "n", desc = "Toggle comment line" },
-		{ "gc", mode = { "n", "o" }, desc = "Toggle comment linewise" },
-		{ "gc", mode = "x", desc = "Toggle comment linewise (visual)" },
-		{ "gbc", mode = "n", desc = "Toggle comment block" },
-		{ "gb", mode = { "n", "o" }, desc = "Toggle comment blockwise" },
-		{ "gb", mode = "x", desc = "Toggle comment blockwise (visual)" },
+	{
+		"numToStr/Comment.nvim",
+		version = false,
+		keys = {
+			{ "gcc", mode = "n", desc = "Toggle comment line" },
+			{ "gc", mode = { "n", "o" }, desc = "Toggle comment linewise" },
+			{ "gc", mode = "x", desc = "Toggle comment linewise (visual)" },
+			{ "gbc", mode = "n", desc = "Toggle comment block" },
+			{ "gb", mode = { "n", "o" }, desc = "Toggle comment blockwise" },
+			{ "gb", mode = "x", desc = "Toggle comment blockwise (visual)" },
+		},
+		opts = {},
 	},
-	opts = {},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		event = "VeryLazy",
+		---@module "todo-comments"
+		---@type TodoOptions
+		opts = {},
+	},
 }
