0ba05d49b9a9885117235fa6de2337d9309d8b03
Juhan280 <azurarobo280@gmail.com>
2025-08-02 21:08:53 +0600

    feat(plugin/git-conflict): use better mappings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,6 +12,14 @@ return { ---@type LazySpec
 		---@type GitConflictUserConfig
 		opts = {
 			default_commands = false,
+			default_mappings = {
+				ours = "<leader>co",
+				theirs = "<leader>ct",
+				none = "<leader>c0",
+				both = "<leader>cb",
+				next = "]x",
+				prev = "[x",
+			},
 		},
 	},
 }
