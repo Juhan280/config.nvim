1572f9cb60d7b1397e96f2f2912b33002c177dd6
Juhan280 <azurarobo280@gmail.com>
2025-08-02 21:07:33 +0600

    remove(plugin/neogit): i don't use it

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,5 @@
 return { ---@type LazySpec
 	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		version = false,
-		cmd = "Neogit",
-		opts = {},
-	},
-	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		opts = { current_line_blame = true },
