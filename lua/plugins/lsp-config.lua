4d707dd559b6e0f60975ec70593ed7a708a2478f
Juhan280 <azurarobo280@gmail.com>
2025-08-12 18:42:05 +0600

    feat(lsp): add termux-language-server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -78,6 +78,27 @@ return { ---@type LazySpec
 				"taplo",
 				"tinymist",
 			})
+
+			-- Setup `termux-language-server`
+			vim.api.nvim_create_autocmd({ "BufEnter" }, {
+				pattern = {
+					"build.sh",
+					"*.subpackage.sh",
+					"PKGBUILD",
+					"*.install",
+					"makepkg.conf",
+					"*.ebuild",
+					"*.eclass",
+					"color.map",
+					"make.conf",
+				},
+				callback = function()
+					vim.lsp.start({
+						name = "termux",
+						cmd = { "termux-language-server" },
+					})
+				end,
+			})
 		end,
 	},
 	{
