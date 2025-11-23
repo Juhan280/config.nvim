4c23a666d6b861d30b3eeed69ec92ea5b6742f0e
Juhan280 <azurarobo280@gmail.com>
2025-11-24 00:11:05 +0600

    fix(tokyonight): make LineNr visible

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -17,6 +17,11 @@ return {
 				-- The default comment is very hard to read
 				colors.comment = "#8080c0"
 			end,
+			on_highlights = function(highlights, colors)
+				highlights.LineNr = { fg = colors.comment }
+				highlights.LineNrAbove = "LineNr"
+				highlights.LineNrBelow = "LineNr"
+			end,
 		})
 		vim.cmd.colorscheme("tokyonight")
 	end,
